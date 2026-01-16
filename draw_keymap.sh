36154a79dca170de0f27a6424c9bcdfe0b39303c
Oktay Konanc <oktay.konanc@gmail.com>
2026-01-16 12:14:17 +0100

    updated drawings

--- draw_keymap.sh
+++ draw_keymap.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
-keymap -c keymap_drawer.config.local.yaml parse -c 12 -z ~/dev/zmk-config-corne/config/corne.keymap >img/corne.yaml
-keymap -c keymap_drawer.config.local.yaml draw img/corne.yaml >img/corne.svg
+ZMKCORNE=~/dev/zmk-config-corne
+keymap -c $ZMKCORNE/keymap_drawer.config.local.yaml parse -c 12 -z $ZMKCORNE/config/corne.keymap > $ZMKCORNE/img/corne.yaml
+keymap -c $ZMKCORNE/keymap_drawer.config.local.yaml draw $ZMKCORNE/img/corne.yaml > $ZMKCORNE/img/corne.svg
